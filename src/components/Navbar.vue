509ed6fad2509552f2929bd5377b970551a07eb3
Hayat Tamboli <hayat.tamboli@gmail.com>
2020-11-09 16:28:24 +0530

    MADE THE BASIC LANDING PAGE

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="nav">
+    <!-- <router-link to="/">Home</router-link> -->
+    <h1 id="icon-and-logo">
+      <img src="../assets/icon.png" alt="Runbhumi" height="32px" />
+      <router-link to="/">Runbhumi</router-link>
+    </h1>
+    <router-link to="/about">Contact us</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#nav {
+  padding: 1rem 8rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  #icon-and-logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: #00adb5;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Navbar></Navbar>
+    <div id="home-body">
+      <div id="text-on-home">
+        <h1 id="main-title">Connect with your nearest sports community</h1>
+        <h3>
+          we provide you with a platform to connect with people of same sports
+          interests and play with them outdoors
+        </h3>
+      </div>
+      <img src="../assets/sports-illustration.svg" alt="" width="50%" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Navbar
   }
 };
 </script>
+
+<style lang="scss" scoped>
+#home-body {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 8rem;
+}
+#main-title {
+  font-family: Montserrat;
+  font-size: 54px;
+  font-weight: 700;
+  font-style: italic;
+  color: #000;
+}
+</style>
